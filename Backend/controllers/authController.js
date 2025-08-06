79a56d056760ed23f3d0067ebb4bcef6a06d8009
Nishant Menaria <nishantmenaria@Mac.local>
2025-08-06 14:26:08 +0530

    a

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -46,8 +46,8 @@ exports.login = async (req, res) => {
   res
     .cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', // use HTTPS in production
-      sameSite: 'strict',
+      secure: true, // use HTTPS in production
+      sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     })
     .json({ message: 'Login successful', user });
