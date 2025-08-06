5c72a91803b435aca20266c891fd458523d34721
Nishant Menaria <nishantmenaria@Mac.local>
2025-08-06 14:36:09 +0530

    a

--- Backend/middlewares/authMiddleware.js
+++ Backend/middlewares/authMiddleware.js
@@ -4,9 +4,9 @@ const User = require('../models/User');
 exports.verifyToken = (req, res, next) => {
   const token = req.cookies.token; 
 
-  if (!token) {
-    return res.status(401).json({ message: 'No token provided in cookies' });
-  }
+  // if (!token) {
+  //   return res.status(401).json({ message: 'No token provided in cookies' });
+  // }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
